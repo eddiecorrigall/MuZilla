483b09722c35cce1a8e0a3a281f4aa0b5a6358a3
Eddie Corrigall <eddiecorrigall@gmail.com>
2015-06-15 19:43:50 -0400

    First working copy

--- src/content.chrome.js
+++ src/content.chrome.js
@@ -10,14 +10,61 @@ chrome.runtime.sendMessage({
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 	if (message.from === 'popup') {
 		switch (message.subject) {
-			case 'getDOM': {
-				sendResponse(document.documentElement.outerHTML);
+			case 'getHTML': {
+				sendResponse(document.documentElement.outerHTML); // document.documentElement.outerHTML
 			} break;
 			case 'getURL': {
 				sendResponse(document.location.href);
 			} break;
-			default: {
-				sendResponse(false);
+			case 'getCommonColor': {
+
+				var allColors = {};
+				var allElements = $(document.getElementsByTagName('*'));
+
+				allElements.each(function() {
+					
+					// Font...
+
+					var fontColor = $(this).css('color');
+					fontColor = tinycolor(fontColor);
+					if (fontColor.isValid() === false) return;
+					fontColor = fontColor.toHexString();
+					fontColor = fontColor.toLowerCase();
+
+					if (!(fontColor in allColors)) {
+						allColors[fontColor] = 0;
+					}
+
+					allColors[fontColor]++;
+
+					/*
+					// Background...
+
+					var backgroundColor = $(this).css('background-color');
+					backgroundColor = tinycolor(backgroundColor);
+					if (backgroundColor.isValid() === false) return;
+					backgroundColor = backgroundColor.toHexString();
+					backgroundColor = backgroundColor.toLowerCase();
+
+					if (!(backgroundColor in allColors)) {
+						allColors[backgroundColor] = 0;
+					}
+
+					allColors[backgroundColor]++;
+					*/
+				});
+
+				var theColor = Object.keys(allColors)[0];
+				var theColorCount = allColors[theColor];
+				
+				$.each(allColors, function(k, v) {
+					if (theColorCount < v) {
+						theColorCount = v;
+						theColor = k;
+					}
+				});
+				
+				sendResponse(theColor);
 			} break;
 		}
 	}
--- src/popup.chrome.js
+++ src/popup.chrome.js
@@ -1,6 +1,69 @@
 console.log("popup.chrome.js");
 
+function getCommonColor(doc) {
+
+	var allColors = {};
+	var allElements = $(doc.getElementsByTagName('*'));
+
+	allElements.each(function() {
+		
+		// Font...
+
+		var fontColor = $(this).css('color');
+		fontColor = tinycolor(fontColor);
+		if (fontColor.isValid() === false) return;
+		fontColor = fontColor.toHexString();
+		fontColor = fontColor.toLowerCase();
+
+		if (!(fontColor in allColors)) {
+			allColors[fontColor] = 0;
+		}
+
+		allColors[fontColor]++;
+
+		// Background...
+
+		var backgroundColor = $(this).css('background-color');
+		backgroundColor = tinycolor(backgroundColor);
+		if (fontColor.isValid() === false) return;
+		backgroundColor = backgroundColor.toHexString();
+		backgroundColor = backgroundColor.toLowerCase();
+
+		if (!(backgroundColor in allColors)) {
+			allColors[backgroundColor] = 0;
+		}
+
+		allColors[backgroundColor]++;
+	});
+
+	var theColor = Object.keys(allColors)[0];
+	var theColorCount = allColors[theColor];
+
+	allColors.each(function(k, v) {
+		if (theColorCount < v) {
+			theColorCount = v;
+			theColor = k;
+		}
+	});
+}
+
 $(document).ready(function() {
+
+	function play() {
+		$('#album_mode')
+			.attr('src', '../assets/play.png')
+			.addClass('play')
+			.removeClass('pause')
+			;
+	}
+
+	function pause() {
+		$('#album_mode')
+			.attr('src', '../assets/pause.png')
+			.addClass('pause')
+			.removeClass('play')
+			;
+	}
 
 	$(document.body).css({
 		'width': 'auto',
@@ -9,7 +72,8 @@ $(document).ready(function() {
 
 	// Query active page...
 
-	$(".album").hide();
+	$('#loader').fadeIn();
+	$('#album').hide();
 
 	chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
 
@@ -18,33 +82,32 @@ $(document).ready(function() {
 		//alert('tab id: ' + activeTab.id);
 
 		/*
-		chrome.tabs.sendMessage(activeTab.id, { from: 'popup', subject: 'getDOM' }, function(response) {
-			alert('msg: ' + response);
-		});
-
 		chrome.tabs.sendMessage(activeTab.id, { from: 'popup', subject: 'getURL' }, function(response) {
 			alert('msg: ' + response);
 		});
 		*/
 
-		// ##### TOM - extract color
-		activeTab.color = "309070";
+		chrome.tabs.sendMessage(activeTab.id, { from: 'popup', subject: 'getCommonColor' }, function(hexColor) {
 
-		// ##### PETER - Fetch album info
-		getAlbumForColor(activeTab.color, activeTab.url, function(error, data) {
+			$('.popup').css('background-color', hexColor);
+			$('#color').text(hexColor);
 
-			if (error) {
-				console.error(error);
-				return;
-			}
+			getAlbumForColor(hexColor.substring(1), activeTab.url, function(error, data) {
 
-			$('#loader').hide();
-			$(".album").fadeIn();
+				if (error) {
+					console.error(error);
+					return;
+				}
 
-			$('#album_image').attr('src', data.art_url);
-			$('#album_artist').text(data.artist);
-			$('#album_title').text(data.name);
-			$('#album_info').text('Todo...');
+				$('#loader').hide();
+				$('#album').fadeIn('slow');
+
+				play();
+
+				$('#album_cover').attr('src', data.art_url);
+				$('#album_artist').text(data.artist);
+				$('#album_title').text(data.name);
+			});
 		});
 	});
 });
